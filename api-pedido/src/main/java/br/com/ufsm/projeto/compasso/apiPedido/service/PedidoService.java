67d066880c1d8ef5ed48fe9bc6d39e368c94373e
igor Vargas <igorvargasp@hotmail.com>
2021-02-11 18:28:10 -0300

    Arrumando o código

--- api-pedido/src/main/java/br/com/ufsm/projeto/compasso/apiPedido/service/PedidoService.java
+++ api-pedido/src/main/java/br/com/ufsm/projeto/compasso/apiPedido/service/PedidoService.java
@@ -30,42 +30,6 @@ public class PedidoService {
 	private ProdutoClient clientProduto;
 
     public Pedido cadastraPedido(Long usuarioId, Long produtoId, Integer quantidade){       
-<<<<<<< HEAD
-=======
-		
-	
-	Integer qtd = quantidade;
-	try {
-		List<Usuario> usuario = clientUser.buscaUsuario();
-		try {
-			LOGGER.info("Entrou no pedido");
-			Pedido pedido = new Pedido();
-			
-			for (Usuario u : usuario) {
-				if(u.getId() == usuarioId) {			
-					pedido.setUsuario(u.getNome());
-					pedido.setIdUsuario(u.getId());
-					LOGGER.info("Adicionou o usuario ao pedido " + u.getId());
-				}
-			}
-			List<Produto> produto = clientProduto.buscaProduto(); 
-			for (Produto p : produto) {
-				if(p.getId() == produtoId) {
-					
-					if(p.getDisponivel().equals(false)){
-						LOGGER.info("Produto indisponivel " + p.getId());
-						return null;
-					}else{
-						pedido.setQuantidade(qtd);
-						pedido.setProduto(p.getNome());
-						pedido.setPreco(p.getPreco());
-						pedido.setIdProduto(p.getId());				
-						LOGGER.info("Adicionou o produto ao pedido " + p.getId());
-						return pedidoRepository.save(pedido);
-					}
-											
-				}
->>>>>>> 962e2b19acc9acb279413bfe8d250eb73aad6aa2
 				
 				
 			}
@@ -96,7 +60,6 @@ public class PedidoService {
 							pedido.setIdUsuario(u.getId());
 							LOGGER.info("Adicionou o usuario ao pedido " + u.getId());
 						}
-<<<<<<< HEAD
 						List<Produto> produto = clientProduto.buscaProduto(); 
 						for (Produto p : produto) {
 							if(p.getId() == produtoId) {
@@ -114,16 +77,6 @@ public class PedidoService {
 								}
 														
 							}
-=======
-					}
-					List<Produto> produto = clientProduto.buscaProduto(); 
-					for (Produto p : produto) {
-						if(p.getId() == produtoId) {
-							pedido.setProduto(p.getNome());
-							pedido.setPreco(p.getPreco());
-							pedido.setIdProduto(p.getId());				
-							LOGGER.info("Adicionou o produto ao pedido " + p.getId());
->>>>>>> 962e2b19acc9acb279413bfe8d250eb73aad6aa2
 						}
 						pedido.setQuantidade(qtd);
 						if (pedido.getIdUsuario().equals(null) && pedido.getIdProduto().equals(null))
@@ -137,14 +90,9 @@ public class PedidoService {
 			} catch (Exception e) {
 				LOGGER.info("Pedido erro " + e);
 			}
-<<<<<<< HEAD
 		
 
-       
+       return null;
 
-=======
-			LOGGER.info("Nada foi cadastrado");
-			return null;
->>>>>>> 962e2b19acc9acb279413bfe8d250eb73aad6aa2
     }
 }
